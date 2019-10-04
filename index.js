6cd11ca6496eefa509271ef4bd059e995cead4f2
Pete Hanner <petehanner@gmail.com>
2019-10-04 09:03:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+class Polygon {
+    constructor(sidesArr) {
+    this._sidesArr = sidesArr
+  }
+
+  get countSides() {
+    return this._sidesArr.length;
+  }
+
+  get perimeter() {
+    // let sum = 0;
+    return this._sidesArr.reduce((m, c) =>  m + c)
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    const orderedSides = this._sidesArr.sort((a, b) => a - b);
+    if (this.countSides !== 3) {
+      return false
+    } else if (orderedSides[0] + orderedSides[1] < orderedSides[2]) {
+      return false
+    } else {
+      return true;
+    }
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    const orderedSides = this._sidesArr.sort((a, b) => a - b);
+
+    if (this.countSides !== 4) {
+      return false;
+    } else if (orderedSides[0] !== orderedSides[3]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  get area() {
+    if (this.isValid) {
+      return this._sidesArr[0] ** 2
+    }
+  }
+}
